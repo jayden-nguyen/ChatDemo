7f2a403ad62c65b5821e2ac80ed13065a2a01b4f
lovebabies <jdnguyen.1995@gmail.com>
2019-01-26 11:50:36 +0700

    login connection success

--- app/src/main/java/com/example/android/chatproject/ApiService.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.chatproject
-
-interface ApiService {
-
-}
---
+++ app/src/main/java/com/example/android/chatproject/model/response/LoginResponseItem.kt
@@ -0,0 +1,8 @@
+package com.example.android.chatproject.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponseItem(@SerializedName("expireTime") val expireTime: Long,
+                             @SerializedName("accessToken") val accessToken: String,
+                             @SerializedName("user") val user: String,
+                             @SerializedName("refreshToken") val refreshToken: String)
